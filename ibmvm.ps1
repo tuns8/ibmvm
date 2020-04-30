7fa618c8a6bcb3ba40dd6e77f950e37f9fbdaba6
tuns8 <tunde.aje@aol.com>
2020-04-30 16:26:55 +0100

    not needd

--- ibmvm.ps1
+++
@@ -1,8 +0,0 @@
- 	
-Get-Disk | Where-Object PartitionStyle –Eq 'RAW' | Initialize-Disk -PartitionStyle MBR
-New-Partition -DiskNumber 2 -Size 32gb -DriveLetter g|
-Format-Volume -FileSystem NTFS -NewFileSystemLabel “TOOLING” -Confirm:$false
-
-New-Partition -DiskNumber 2 -Size 128gb -DriveLetter f|
-Format-Volume -FileSystem NTFS -NewFileSystemLabel “DOCKER” -Confirm:$false
- 
